c7e68a3ed6d88043d0e45d32590d9f7abc2d7860
Kerry Buckley <kerryjbuckley@gmail.com>
2024-05-30 11:33:03 +0100

    Allow counter to be run and total updated

--- lib/parallel_sum_gui_web/live/home_live.ex
+++ lib/parallel_sum_gui_web/live/home_live.ex
@@ -2,17 +2,45 @@ defmodule ParallelSumGuiWeb.HomeLive do
   @moduledoc false
   use ParallelSumGuiWeb, :live_view
 
+  alias ParallelSum.Counter
+  alias ParallelSum.DummyService
+  alias ParallelSum.Runner
   alias ParallelSum.Total
   alias Phoenix.LiveView
 
   @impl LiveView
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, total: Total.value())}
+    {:ok,
+     socket
+     |> assign(form: to_form(%{"tasks" => 100}))
+     |> assign_total()}
   end
 
   @impl LiveView
   def handle_event("reset", _unsigned_params, socket) do
+    Counter.reset()
     Total.reset()
-    {:noreply, assign(socket, total: Total.value())}
+    {:noreply, assign_total(socket)}
+  end
+
+  def handle_event("submit", %{"tasks" => tasks}, socket) do
+    view_pid = self()
+    Task.start(fn -> call_runner(String.to_integer(tasks), view_pid) end)
+    {:noreply, socket}
+  end
+
+  @impl LiveView
+  def handle_info(:total_updated, socket), do: {:noreply, assign_total(socket)}
+
+  defp assign_total(socket), do: assign(socket, total: Total.value())
+
+  defp call_runner(tasks, view_pid) do
+    Runner.run(fn -> run_and_notify(view_pid) end, tasks)
+  end
+
+  def run_and_notify(view_pid) do
+    total = DummyService.run()
+    send(view_pid, :total_updated)
+    total
   end
 end
--- test/parallel_sum_gui_web/live/home_live_test.exs
+++ test/parallel_sum_gui_web/live/home_live_test.exs
@@ -3,10 +3,12 @@ defmodule ParallelSumGuiWeb.HomeLiveTest do
 
   import Phoenix.LiveViewTest
 
+  alias ParallelSum.Counter
   alias ParallelSum.Total
 
   describe "ParallelSumGuiWeb.HomeLive" do
     setup do
+      Counter.reset()
       Total.reset()
     end
 
@@ -15,12 +17,33 @@ defmodule ParallelSumGuiWeb.HomeLiveTest do
       assert view |> element("#total", "0") |> has_element?()
     end
 
-    test "allows the total to be reset", %{conn: conn} do
+    test "allows the counter to be run", %{conn: conn} do
+      {:ok, view, _html} = live(conn, ~p"/")
+      view |> element("form") |> render_submit(%{"tasks" => "100"})
+      assert view |> element("#total", "500500") |> eventually_has_element?()
+    end
+
+    test "allows the counter and total to be reset", %{conn: conn} do
+      Counter.value()
       Total.add(42)
       {:ok, view, _html} = live(conn, ~p"/")
       assert view |> element("#total", "42") |> has_element?()
       view |> element("#reset") |> render_click()
+      assert Counter.value() == 1
+      assert Total.value() == 0
       assert view |> element("#total", "0") |> has_element?()
+    end
+  end
+
+  defp eventually_has_element?(element, retries \\ 0)
+  defp eventually_has_element?(_element, 200), do: false
+
+  defp eventually_has_element?(element, retries) do
+    if has_element?(element) do
+      true
+    else
+      Process.sleep(100)
+      eventually_has_element?(element, retries + 1)
     end
   end
 end
