1cd77d1237a963d7a029e02250c057a16d26bbc0
Kerry Buckley <kerryjbuckley@gmail.com>
2024-05-30 09:48:05 +0100

    Add reset button and show real total

--- lib/parallel_sum_gui_web/live/home_live.ex
+++ lib/parallel_sum_gui_web/live/home_live.ex
@@ -2,7 +2,17 @@ defmodule ParallelSumGuiWeb.HomeLive do
   @moduledoc false
   use ParallelSumGuiWeb, :live_view
 
+  alias ParallelSum.Total
+  alias Phoenix.LiveView
+
+  @impl LiveView
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, total: 0)}
+    {:ok, assign(socket, total: Total.value())}
+  end
+
+  @impl LiveView
+  def handle_event("reset", _unsigned_params, socket) do
+    Total.reset()
+    {:noreply, assign(socket, total: Total.value())}
   end
 end
--- test/parallel_sum_gui_web/live/home_live_test.exs
+++ test/parallel_sum_gui_web/live/home_live_test.exs
@@ -3,9 +3,23 @@ defmodule ParallelSumGuiWeb.HomeLiveTest do
 
   import Phoenix.LiveViewTest
 
+  alias ParallelSum.Total
+
   describe "ParallelSumGuiWeb.HomeLive" do
+    setup do
+      Total.reset()
+    end
+
     test "shows the current total", %{conn: conn} do
       {:ok, view, _html} = live(conn, ~p"/")
+      assert view |> element("#total", "0") |> has_element?()
+    end
+
+    test "allows the total to be reset", %{conn: conn} do
+      Total.add(42)
+      {:ok, view, _html} = live(conn, ~p"/")
+      assert view |> element("#total", "42") |> has_element?()
+      view |> element("#reset") |> render_click()
       assert view |> element("#total", "0") |> has_element?()
     end
   end
